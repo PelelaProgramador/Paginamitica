e59ea5b89403c8e2bd384c4700a0a56e7360abbc
PelelaProgramador <lucas.barrazabr@gmail.com>
2023-05-03 22:54:13 -0400

    Initial commit

---
+++ js/kkk.js
@@ -0,0 +1,35 @@
+
+  function reproducirMusica() {
+  audio.play();
+  
+  }
+// Reproducir la canción seleccionada
+function playAudio() {
+  audio.src = songSelect.value;
+  audio.play();
+}
+
+
+
+
+
+
+// Cambiar el volumen
+function setVolume() {
+  audio.volume = volumeSlider.value;
+}
+
+
+           
+function cambiarImagen() {
+  var imagen = document.getElementById("imagen");
+  if (imagen.src.match("img/Amongus.png")) {
+    imagen.src = "img/pumtoma.jpg";
+    var audio = new Audio("img/roca.mp3");
+    audio.play();
+  } else {
+    imagen.src = "img/Amongus.png";
+    var audio = new Audio("img/Amongus.mp3");
+    audio.play();
+  }
+}
